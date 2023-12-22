639092c0f44b74c27fd680e4a97d1a2820b93256
Max Nadein <m.g.nadyein@student.khai.edu>
2023-12-22 14:04:47 +0200

    У цьому коді в методі main додано декілька викликів history.addSongToHistory("SongX") для додавання пісень до історії перед виведенням історії.

--- src/edu/khai/lab2/SearchHistory.java
+++ src/edu/khai/lab2/SearchHistory.java
@@ -1,48 +1,52 @@
 package edu.khai.lab2;
+
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 class SearchHistory {
-    // Додано приватне поле з колекцією (Map)
     private Map<String, LocalDateTime> songsSearched;
 
     SearchHistory() {
-        // Ініціалізація колекції в конструкторі (використовуючи HashMap)
         songsSearched = new HashMap<>();
     }
 
-    // Метод додавання пісні до колекції
     void addSongToHistory(String songTitle) {
-        // Додавання назви пісні та дати створення до Map
         songsSearched.put(songTitle, LocalDateTime.now());
     }
 
-    // Метод отримання дати створення пісні з колекції
     LocalDateTime getCreationDate(String songTitle) {
         return songsSearched.get(songTitle);
     }
 
-    // Метод перевірки наявності пісні у колекції
     boolean containsSong(String songTitle) {
         return songsSearched.containsKey(songTitle);
     }
 
-    // Метод видалення пісні з колекції
     void removeSongFromHistory(String songTitle) {
         songsSearched.remove(songTitle);
     }
 
-    // Метод очищення історії
     void clearHistory() {
         songsSearched.clear();
     }
 
-    // Метод виведення історії
     void displayHistory() {
         System.out.println("Search History:");
         for (Map.Entry<String, LocalDateTime> entry : songsSearched.entrySet()) {
             System.out.println("Song: " + entry.getKey() + ", Creation Date: " + entry.getValue());
         }
+    }
+
+    public static void main(String[] args) {
+        SearchHistory history = new SearchHistory();
+
+        // Додавання пісень до історії
+        history.addSongToHistory("Song1");
+        history.addSongToHistory("Song2");
+        history.addSongToHistory("Song3");
+
+        // Виведення історії
+        history.displayHistory();
     }
 }
